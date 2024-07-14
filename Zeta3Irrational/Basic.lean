c4e559075415736d1bf18c7a7207d18ae7d8a11b
ahhwuhu <ljq_0209@tju.edu.cn>
2024-07-14 13:42:07 +0800

    Zeta3Irrational/Basic.leanZeta3Irrational/LegendrePoly.leanZeta3Irrational/LinearForm.lean

--- Zeta3Irrational/Basic.lean
+++ Zeta3Irrational/Basic.lean
@@ -8,181 +8,10 @@ import Zeta3Irrational.Integral
 import Zeta3Irrational.Equality
 import Zeta3Irrational.LegendrePoly
 import Zeta3Irrational.Bound
+import Zeta3Irrational.LinearForm
 
 open scoped Nat
-open BigOperators
-
-namespace LinearForm
-
-lemma d_cube_ne_zero {r : ℕ} : ((d (Finset.Icc 1 r) ^ 3) : ℝ) ≠ (0 : ℝ) := by
-  simp only [ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, pow_eq_zero_iff, Nat.cast_eq_zero]
-  apply d_ne_zero; simp
-
-lemma J_rr_linear (r : ℕ) :
-    ∃ a : ℤ, J r r = 2 * ∑' n : ℕ , 1 / ((n : ℝ) + 1) ^ 3 - a / (d (Finset.Icc 1 r)) ^ 3 := by
-  if h : r = 0 then
-    rw [h, zeta_3]; use 0; simp
-  else
-    rw [J_rr (by omega)]
-    simp only [sub_right_inj]
-    simp_rw [eq_div_iff d_cube_ne_zero, Finset.mul_sum, Finset.sum_mul]
-    use ∑ i ∈ Finset.Icc 1 r, 2 * ↑(d (Finset.Icc 1 r)) ^ 3 / ↑i ^ 3
-    simp only [Int.cast_sum, Int.cast_mul, Int.cast_ofNat, Int.cast_pow, Int.cast_natCast]
-    apply Finset.sum_congr rfl
-    intro x hx
-    rw [mul_assoc]
-    nth_rewrite 2 [mul_comm]
-    rw [mul_one_div, Int.cast_div]
-    · simp only [Int.cast_mul, Int.cast_ofNat, Int.cast_pow, Int.cast_natCast, mul_div_assoc']
-    · rw [← Nat.cast_pow, ← Nat.cast_pow, d_cube', Nat.cast_pow]
-      apply Dvd.dvd.mul_left
-      norm_cast
-      apply dvd_d_of_mem
-      simp_all
-    · simp_all only [Finset.mem_Icc, Int.cast_pow, Int.cast_natCast, ne_eq, OfNat.ofNat_ne_zero,
-      not_false_eq_true, pow_eq_zero_iff, Nat.cast_eq_zero]
-      linarith
-
-lemma Icc_diff_Icc {r s : ℕ} (h : r > s) (g : ¬s = 0) : Finset.Icc 1 r \ Finset.Icc 1 s = Finset.Icc (s + 1) r := by
-  ext x
-  constructor
-  · intro hx
-    simp_all only [gt_iff_lt, Finset.mem_sdiff, Finset.mem_Icc, not_and, not_le, and_true]
-    exact LT.lt.nat_succ_le (hx.2 hx.1.1)
-  · intro hx
-    simp_all only [gt_iff_lt, Finset.mem_Icc, Finset.mem_sdiff, and_true, not_and, not_le]
-    constructor
-    · linarith
-    · intro
-      linarith
-
-lemma one_div_sum_eq {r s : ℕ} (h : r > s) :
-    ∑ m ∈ Finset.Icc 1 r, (1 / m ^ 2 : ℝ) - ∑ m ∈ Finset.Icc 1 s, (1 / m ^ 2 : ℝ) =
-    ∑ m ∈ Finset.Icc 1 (r - s), (1 / (s + m) ^ 2 : ℝ) := by
-  rw [← Finset.sum_sdiff_eq_sub]
-  · if h1 : s = 0 then
-      simp_all
-    else
-      simp_rw [← Nat.cast_add]
-      rw [Icc_diff_Icc h h1, ← Nat.Ico_succ_right, ← Nat.Ico_succ_right,
-        Finset.sum_Ico_add (f := fun (x : ℕ) => 1 / ((x ^ 2) : ℝ)) (c := s), Nat.succ_eq_add_one,
-        Nat.succ_eq_add_one, add_comm, show r - s + 1 + s = r + 1 by omega]
-  · exact Finset.Icc_subset_Icc_right (LT.lt.le (gt_iff_lt.1 h))
-
-lemma J_rs_linear {r s : ℕ} (h : r > s) : ∃ a : ℤ, J r s = a / (d (Finset.Icc 1 r)) ^ 3 := by
-  rw [J_rs (by linarith)]
-  simp_rw [eq_div_iff d_cube_ne_zero, one_div_sum_eq h]
-  use (∑ m ∈ Finset.Icc 1 (r - s), (d (Finset.Icc 1 r)) ^ 2 / (s + m) ^ 2 *
-    (d (Finset.Icc 1 r)) / (r - s))
-  rw [Finset.sum_div, Finset.sum_mul, Int.cast_sum]
-  apply Finset.sum_congr rfl
-  intro i hi
-  rw [show 3 = 1 + 2 by omega, pow_add, pow_one, mul_comm, mul_assoc, mul_div_assoc', mul_one_div,
-    mul_div_assoc', mul_comm, ← mul_div_assoc', Int.cast_div]
-  · rw [Int.cast_mul, Int.cast_div]
-    · simp_all only [gt_iff_lt, Finset.mem_Icc, Int.cast_pow, Int.cast_natCast, Int.cast_add,
-      Int.cast_sub]
-      rw [mul_div_assoc']
-    · norm_cast
-      rw [d_sq']
-      apply dvd_d_of_mem
-      simp_all only [gt_iff_lt, Finset.mem_Icc, Finset.mem_image, ge_iff_le, zero_le, ne_eq,
-        OfNat.ofNat_ne_zero, not_false_eq_true, pow_left_inj, exists_eq_right]
-      omega
-    · simp_all only [gt_iff_lt, Finset.mem_Icc, Int.cast_pow, Int.cast_add, Int.cast_natCast,
-      ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, pow_eq_zero_iff]
-      rw [← Nat.cast_add]
-      norm_cast; omega
-  · apply Dvd.dvd.mul_left
-    rw [← Nat.cast_sub (by linarith)]
-    norm_cast
-    apply dvd_d_of_mem
-    simp_all only [gt_iff_lt, Finset.mem_Icc, tsub_le_iff_right, le_add_iff_nonneg_right, zero_le,
-      and_true]
-    linarith
-  · simp_all only [gt_iff_lt, Finset.mem_Icc, Int.cast_sub, Int.cast_natCast]
-    rw [← Nat.cast_sub (by linarith)]
-    norm_cast; linarith
-
-lemma multi_integral_sum_comm (c : ℕ → ℤ) : ∫ (x : ℝ) (y : ℝ) in (0)..1, ∑ x_1 ∈ Finset.range (n + 1),
-    ∑ x_2 ∈ Finset.range (n + 1), ↑(c x_1) * x ^ x_1 * ↑(c x_2) * y ^ x_2 * (x * y).log / (1 - x * y)
-    = ∑ x_1 ∈ Finset.range (n + 1), ∑ x_2 ∈ Finset.range (n + 1), ∫ (x : ℝ) (y : ℝ) in (0)..1,
-    ↑(c x_1) * x ^ x_1 * ↑(c x_2) * y ^ x_2 * (x * y).log / (1 - x * y) := by
-  sorry
-
-lemma multi_integral_mul_const (c d : ℕ) (p q : ℝ): ∫ (x : ℝ) (y : ℝ) in (0)..1,
-    p * x ^ c * q * y ^ d * (x * y).log / (1 - x * y) = p * q * - J c d := by
-  simp_rw [mul_div_assoc, J, neg_neg]
-  rw [← intervalIntegral.integral_const_mul]
-  simp_rw [← intervalIntegral.integral_const_mul]
-  apply intervalIntegral.integral_congr
-  intro x _
-  apply intervalIntegral.integral_congr
-  intro y _
-  ring
-
-noncomputable def p (r s : ℕ) : ℤ :=
-  if h : r > s then (J_rs_linear h).choose
-  else if h : r < s then (J_rs_linear h).choose
-  else -(J_rr_linear r).choose
-
-noncomputable def q (r s : ℕ) : ℤ :=
-  if r > s then 0
-  else if r < s then 0
-  else 2
-
-lemma J_symm (r s : ℕ) : J r s = J s r := by
-  rw [neg_inj]
-  have : ∫ (x : ℝ) in (0)..1, ∫ (y : ℝ) in (0)..1, x ^ r * y ^ s * (x * y).log / (1 - x * y) =
-    ∫ (y : ℝ) in (0)..1, ∫ (x : ℝ) in (0)..1, x ^ r * y ^ s * (x * y).log / (1 - x * y) := by
-    rw [intervalIntegral.integral_of_le (by norm_num), intervalIntegral.integral_of_le (by norm_num),
-    MeasureTheory.integral_Ioc_eq_integral_Ioo, MeasureTheory.integral_Ioc_eq_integral_Ioo]
-    rw [← MeasureTheory.integral_indicator (by simp), ← MeasureTheory.integral_indicator (by simp)]
-    sorry
-  rw [this]
-  apply intervalIntegral.integral_congr
-  intro _ _
-  simp
-  apply intervalIntegral.integral_congr
-  intro _ _
-  simp
-  ring_nf
-
-lemma linear_int_aux : ∃ a b : ℕ → ℕ → ℤ, ∀ r s : ℕ, J r s =
-    b r s * ∑' n : ℕ , 1 / ((n : ℝ) + 1) ^ 3 + a r s / (d (Finset.Icc 1 (Nat.max r s))) ^ 3 := by
-  use p
-  use q
-  intro x y
-  if h : x > y then
-    cases' (J_rs_linear h) with a ha
-    simp only [p, q]
-    simp_all only [gt_iff_lt, one_div, dite_eq_ite, ite_true, Int.cast_zero, zero_mul, dite_true,
-      zero_add]
-    rw [(J_rs_linear h).choose_spec] at ha
-    rw [show x.max y = x by exact max_eq_left_iff.2 (LT.lt.le h)]
-    simp_all
-  else if h1 : x < y then
-    cases' (J_rs_linear h1) with a ha
-    simp only [p, q]
-    obtain h2 := J_symm x y
-    simp_all only [gt_iff_lt, one_div, dite_eq_ite, ite_true, ite_self, Int.cast_zero,
-      zero_mul, dite_true, zero_add, not_false_eq_true, dite_false]
-    rw [(J_rs_linear h1).choose_spec] at ha
-    simp only [not_lt] at h
-    simp_all
-  else
-    have h : x = y := by linarith
-    cases' (J_rr_linear y) with a ha
-    simp only [p, q]
-    simp_all only [gt_iff_lt, lt_self_iff_false, not_false_eq_true, dite_eq_ite, ite_false,
-      Int.cast_ofNat, not_isEmpty_of_nonempty, tsum_empty, mul_zero, zero_sub, sub_right_inj,
-      dite_false, Int.cast_neg, max_self]
-    rw [(J_rr_linear y).choose_spec, ← Mathlib.Tactic.RingNF.add_neg, ← neg_div] at ha
-    simp_all
-
-end LinearForm
-
-open Polynomial LinearForm
+open BigOperators Polynomial
 
 noncomputable abbrev JJ (n : ℕ) : ℝ :=
   - ∫ (x : ℝ) in (0)..1, (∫ (y : ℝ) in (0)..1,
@@ -243,10 +72,6 @@ lemma linear_int (n : ℕ) : ∃ a b : ℕ → ℤ,
       simp
   · ring
 
-lemma legendre_integral_special (a : ℝ) : ∫ (x : ℝ) in (0)..1, eval x (legendre n) / (1 - a * x) =
-    (-1) ^ n * ∫ (x : ℝ) in (0)..1, x ^ n * (1 - x) ^ n * a ^ n / (1 - a * x) ^ (n + 1) := by
-  sorry
-
 lemma JJ_eq_form (n : ℕ) : JJ n = ∫ (x : ℝ) (y : ℝ) (z : ℝ) in (0)..1,
     ( x * (1 - x) * y * (1 - y) * z * (1 - z) / ((1 - (1 - z) * x) * (1 - y * z))) ^ n / ((1 - (1 - z) * x) * (1 - y * z)) := by
   calc
@@ -270,7 +95,7 @@ lemma JJ_eq_form (n : ℕ) : JJ n = ∫ (x : ℝ) (y : ℝ) (z : ℝ) in (0)..1,
       (∫ (y : ℝ) in (0)..1, eval y (legendre n) / ((1 - y * z))) := by
     rw [intervalIntegral.integral_of_le (by norm_num), intervalIntegral.integral_of_le (by norm_num),
       MeasureTheory.integral_Ioc_eq_integral_Ioo, MeasureTheory.integral_Ioc_eq_integral_Ioo]
-    apply  MeasureTheory.setIntegral_congr (by simp)
+    apply MeasureTheory.setIntegral_congr (by simp)
     intro z hz
     simp only
     rw [← intervalIntegral.integral_mul_const]
@@ -283,7 +108,9 @@ lemma JJ_eq_form (n : ℕ) : JJ n = ∫ (x : ℝ) (y : ℝ) (z : ℝ) in (0)..1,
       MeasureTheory.integral_Ioc_eq_integral_Ioo, MeasureTheory.integral_Ioc_eq_integral_Ioo]
     apply  MeasureTheory.setIntegral_congr (by simp)
     intro z hz
-    simp_rw [legendre_integral_special, mul_comm, legendre_integral_special]
+    simp only [Set.mem_Ioo] at hz
+    have hz1 : 0 < (1 - z) ∧ (1 - z) < 1 := by constructor <;> linarith
+    simp_rw [legendre_integral_special hz1, mul_comm, legendre_integral_special hz]
     rw [mul_mul_mul_comm, ← pow_add, ← two_mul, pow_mul]
     simp only [even_two, Even.neg_pow, one_pow, one_mul, div_pow]
     simp_rw [← intervalIntegral.integral_const_mul]
@@ -330,10 +157,10 @@ lemma IntervalIntegrable2 {x : ℝ} (hx : x ∈ Set.Ioo 0 1) : IntervalIntegrabl
   apply Continuous.intervalIntegrable
   simp_rw [intervalIntegral.integral_of_le (show 0 ≤ 1 by norm_num), ← MeasureTheory.integral_Icc_eq_integral_Ioc]
   apply continuous_parametric_integral_of_continuous
-  · sorry
+  · rw [Function.uncurry_def]
+    sorry
   · rw [← Set.uIcc_of_le (by norm_num)]
     exact isCompact_uIcc
-
 
 lemma IntervalIntegrable3 {x y : ℝ} (hx : x ∈ Set.Ioo 0 1) (hy : y ∈ Set.Ioo 0 1): IntervalIntegrable
   (fun z ↦ (x * (1 - x) * y * (1 - y) * z * (1 - z) / ((1 - (1 - z) * x) * (1 - y * z))) ^ n /
--- Zeta3Irrational/LegendrePoly.lean
+++ Zeta3Irrational/LegendrePoly.lean
@@ -126,11 +126,259 @@ lemma legendre_eval_symm {n : ℕ} {x : ℝ} : eval x (legendre n) =
   rw [← pow_add, show i + n = n - i + 2 * i by omega, pow_add]
   simp
 
--- lemma integral_legendre_mul_smooth_eq {n : ℕ} (f : ℝ → ℝ) :
---   ∫ (x : ℝ) in (0)..1, eval x (legendre n) * f x =
---   (- 1) ^ n / n ! * ∫ (x : ℝ) in (0)..1, x ^ n * (1 - x) ^ n * (Fderivative^[n] f) x := by
---   simp only [eval_mul, one_div, eval_C]
---   induction' n with n hn
---   · simp
---   · rw [← intervalIntegral.integral_const_mul]
---     sorry
+lemma n_derivative {a : ℝ} (n : ℕ): deriv^[n] (fun x ↦ 1 / (1 - a * x)) =
+    (fun x ↦ (n) ! * (a ^ n) / (1 - a * x) ^ (n + 1)) := by
+  induction' n with n hn
+  · simp
+  · rw [Function.iterate_succ_apply', hn]
+    ext x
+    if h : 1 - a * x = 0 then
+      rw [h]
+      simp only [differentiableAt_const, ne_eq, add_eq_zero, one_ne_zero, and_false, and_self,
+        not_false_eq_true, zero_pow, div_zero]
+      apply IsLocalExtr.deriv_eq_zero
+      left
+
+      sorry
+    else
+      -- rw [deriv_div]
+      -- simp only [differentiableAt_const, deriv_mul, deriv_const', zero_mul, deriv_pow'', mul_zero,
+      --   add_zero, zero_sub]
+      sorry
+
+lemma legendre_poly_eval_zero_eq_zero {m : ℕ} (h : m < n) :
+    eval 0 ((⇑derivative)^[m] (X ^ n * (1 - X) ^ n) : ℝ[X]) = 0 := by
+  rw [Polynomial.iterate_derivative_mul, Polynomial.eval_finset_sum]
+  apply Finset.sum_eq_zero
+  intro x hx
+  simp_all only [Nat.succ_eq_add_one, Finset.mem_range, nsmul_eq_mul, eval_mul, eval_natCast,
+    mul_eq_zero, Nat.cast_eq_zero]
+  right; left
+  simp only [Polynomial.iterate_derivative_X_pow_eq_smul, eval_smul, eval_pow, eval_X, smul_eq_mul,
+    mul_eq_zero, Nat.cast_eq_zero, Nat.descFactorial_eq_zero_iff_lt, pow_eq_zero_iff', ne_eq, true_and]
+  right
+  suffices n - (m - x) > 0 by linarith
+  simp only [gt_iff_lt, tsub_pos_iff_lt]
+  rw [Nat.lt_add_one_iff] at hx
+  calc
+    m - x ≤ m := by simp
+    _ < n := by exact h
+
+lemma legendre_poly_eval_one_eq_zero {m : ℕ} (h : m < n) :
+    eval 1 ((⇑derivative)^[m] (X ^ n * (1 - X) ^ n) : ℝ[X]) = 0 := by
+  rw [Polynomial.iterate_derivative_mul, Polynomial.eval_finset_sum]
+  apply Finset.sum_eq_zero
+  intro x hx
+  simp_all only [Nat.succ_eq_add_one, Finset.mem_range, nsmul_eq_mul, eval_mul, eval_natCast,
+    mul_eq_zero, Nat.cast_eq_zero]
+  right; right
+  rw [show (1 - X : ℝ[X]) ^ n = (X ^ n : ℝ[X]).comp (1 - X) by simp,
+    Polynomial.iterate_derivative_comp_one_sub_X (p := X ^ n),
+    Polynomial.iterate_derivative_X_pow_eq_smul]
+  simp only [smul_comp, pow_comp, X_comp, Algebra.mul_smul_comm, eval_smul, eval_mul, eval_pow,
+    eval_neg, eval_one, eval_sub, eval_X, sub_self, smul_eq_mul, mul_eq_zero, Nat.cast_eq_zero,
+    Nat.descFactorial_eq_zero_iff_lt, pow_eq_zero_iff', neg_eq_zero, one_ne_zero, ne_eq, false_and,
+    true_and, false_or]
+  right
+  suffices n - x > 0 by linarith
+  simp only [gt_iff_lt, tsub_pos_iff_lt]
+  linarith
+
+lemma integral_legendre_mul_smooth_eq_aux {n : ℕ} {a : ℝ} (m : ℕ) (h : m ≤ n) (ha : 0 < a ∧ a < 1):
+    ∫ (x : ℝ) in (0)..1, eval x ((⇑derivative)^[n] (X ^ n * (1 - X) ^ n)) * (fun x ↦ 1 / (1 - a * x)) x =
+    (-1) ^ m * ∫ (x : ℝ) in (0)..1, eval x ((⇑derivative)^[n - m] (X ^ n * (1 - X) ^ n)) *
+    (deriv^[m] fun x ↦ 1 / (1 - a * x)) x := by
+  induction' m with m hm
+  · simp
+  · have h₀ : m < n := by linarith
+    have h₁ : n - (m + 1) < n := by omega
+    rw [hm (LT.lt.le h₀), pow_add, pow_one, mul_assoc]
+    congr
+    symm
+    rw [show n - m = n - (m + 1) + 1 by omega, Function.iterate_succ_apply']
+    rw [neg_one_mul, neg_eq_iff_eq_neg, intervalIntegral.integral_mul_deriv_eq_deriv_mul_of_hasDerivAt
+      (u := fun x ↦ eval x ((⇑derivative)^[n - (m + 1)] (X ^ n * (1 - X) ^ n : ℝ[X])))
+      (u' := fun x ↦ eval x (derivative ((⇑derivative)^[n - (m + 1)] (X ^ n * (1 - X) ^ n : ℝ[X]))))
+      (v := fun x ↦ (deriv^[m] fun x ↦ 1 / (1 - a * x)) x)]
+    · rw [legendre_poly_eval_one_eq_zero h₁, legendre_poly_eval_zero_eq_zero h₁]
+      simp only [zero_mul, sub_self, zero_sub, Function.iterate_succ_apply']
+    · simp_rw [Polynomial.iterate_derivative_mul, Polynomial.eval_finset_sum]
+      apply continuousOn_finset_sum
+      intro i _
+      simp_rw [Algebra.smul_def, eval_mul]
+      apply ContinuousOn.mul
+      · simp only [eq_natCast, eval_natCast, ge_iff_le, zero_le_one, Set.uIcc_of_le]
+        apply continuousOn_const
+      · apply ContinuousOn.mul
+        · simp_rw [Polynomial.iterate_derivative_X_pow_eq_smul]
+          simp only [eval_smul, eval_pow, eval_X, smul_eq_mul, ge_iff_le, zero_le_one,
+            Set.uIcc_of_le]
+          apply ContinuousOn.mul continuousOn_const (ContinuousOn.pow continuousOn_id _)
+        · rw [show (1 - X : ℝ[X]) ^ n = (X ^ n : ℝ[X]).comp (1 - X) by simp,
+            Polynomial.iterate_derivative_comp_one_sub_X (p := X ^ n),
+            Polynomial.iterate_derivative_X_pow_eq_smul]
+          simp only [smul_comp, pow_comp, X_comp, Algebra.mul_smul_comm, eval_smul, eval_mul,
+            eval_pow, eval_neg, eval_one, eval_sub, eval_X, smul_eq_mul, ge_iff_le, zero_le_one,
+            Set.uIcc_of_le]
+          refine ContinuousOn.mul continuousOn_const (ContinuousOn.mul continuousOn_const ?_)
+          apply ContinuousOn.pow (ContinuousOn.sub continuousOn_const continuousOn_id)
+    · simp_rw [n_derivative]
+      apply ContinuousOn.div continuousOn_const
+      · apply ContinuousOn.pow (ContinuousOn.sub continuousOn_const (ContinuousOn.mul continuousOn_const continuousOn_id))
+      · intro x hx
+        simp only [ge_iff_le, zero_le_one, Set.uIcc_of_le, Set.mem_Icc] at hx
+        suffices (1 - a * x) ^ (m + 1) > 0 by linarith
+        apply pow_pos
+        nlinarith
+    · intro x _
+      simp_rw [Polynomial.iterate_derivative_mul, Polynomial.eval_finset_sum]
+      simp only [Nat.succ_eq_add_one, nsmul_eq_mul, eval_mul, eval_natCast, map_sum, eval_finset_sum]
+      apply HasDerivAt.sum
+      intro i _
+      simp only [derivative_mul, derivative_natCast, zero_mul, zero_add, eval_mul, eval_natCast,
+        eval_add]
+      apply HasDerivAt.const_mul
+      apply HasDerivAt.mul
+      · rw [Polynomial.iterate_derivative_X_pow_eq_smul]
+        simp only [eval_smul, eval_pow, eval_X, smul_eq_mul, LinearMapClass.map_smul,
+          derivative_X_pow, map_natCast, eval_mul, eval_natCast]
+        apply HasDerivAt.const_mul
+        apply hasDerivAt_pow
+      · rw [show (1 - X : ℝ[X]) ^ n = (X ^ n : ℝ[X]).comp (1 - X) by simp,
+          Polynomial.iterate_derivative_comp_one_sub_X (p := X ^ n),
+          Polynomial.iterate_derivative_X_pow_eq_smul]
+        simp only [smul_comp, pow_comp, X_comp, Algebra.mul_smul_comm, eval_smul, eval_mul,
+          eval_pow, eval_neg, eval_one, eval_sub, eval_X, smul_eq_mul, LinearMapClass.map_smul]
+        apply HasDerivAt.const_mul
+        simp only [derivative_mul, eval_add, eval_mul, eval_pow, eval_sub, eval_one, eval_X,
+          eval_neg]
+        have h1 : (derivative ((-1) ^ i : ℝ[X])) = 0 := by
+          rw [show ((-1) ^ i : ℝ[X]) = C ((-1) ^ i) by simp, derivative_C]
+        rw [h1, eval_zero, zero_mul, zero_add]
+        apply HasDerivAt.const_mul
+        rw [show (1 - X : ℝ[X]) ^ (n - i) = (X ^ (n - i) : ℝ[X]).comp (1 - X) by simp,
+          Polynomial.derivative_comp_one_sub_X (p := X ^ (n - i)),
+          Polynomial.derivative_X_pow]
+        simp only [mul_comp, pow_comp, X_comp, eval_mul, map_natCast, natCast_comp, eval_neg,
+          eval_mul, eval_natCast, eval_pow, eval_sub, eval_one, eval_X]
+        rw [← mul_neg_one]
+        apply HasDerivAt.pow
+        apply HasDerivAt.const_sub
+        apply hasDerivAt_id
+    · intro x hx
+      simp only [ge_iff_le, zero_le_one, min_eq_left, max_eq_right, Set.mem_Ioo] at hx
+      have (x : ℝ) : deriv (deriv^[m] fun x ↦ 1 / (1 - a * x)) x = Function.eval x (deriv (deriv^[m] fun x ↦ 1 / (1 - a * x))) := by
+        simp only [one_div, Function.eval]
+      simp_rw [this]
+      simp_rw [← Function.iterate_succ_apply', Nat.succ_eq_add_one, Function.eval, n_derivative]
+      have : ↑(m + 1)! * a ^ (m + 1) / (1 - a * x) ^ (m + 1 + 1) =
+          ((deriv (fun _ ↦ (↑m ! * a ^ m : ℝ)) x) * (1 - a * x) ^ (m + 1) - (↑m ! * a ^ m : ℝ) *
+          deriv (fun x ↦ (1 - a * x) ^ (m + 1)) x) / ((1 - a * x) ^ (m + 1)) ^ 2:= by
+        rw [div_eq_div_iff]
+        · simp only [differentiableAt_const, deriv_mul, deriv_const', zero_mul, deriv_pow'',
+          mul_zero, add_zero, zero_sub]
+          rw [← neg_mul, deriv_pow'', deriv_const_sub , deriv_const_mul]
+          · simp only [Nat.cast_add, Nat.cast_one, add_tsub_cancel_right, deriv_id'', mul_one,
+              mul_neg, neg_mul, neg_neg]
+            norm_cast
+            rw [Nat.factorial_succ, ← pow_mul', Nat.cast_mul]
+            ring
+          · apply differentiableAt_id
+          · apply DifferentiableAt.const_sub
+            apply DifferentiableAt.const_mul differentiableAt_id
+        · suffices (1 - a * x) ^ (m + 1 + 1) > 0 by linarith
+          apply pow_pos
+          nlinarith
+        · suffices (1 - a * x) ^ (m + 1) > 0 by nlinarith
+          apply pow_pos
+          nlinarith
+      rw [this]
+      apply HasDerivAt.div
+      · simp only [differentiableAt_const, deriv_mul, deriv_const', zero_mul, deriv_pow'',
+        mul_zero, add_zero]
+        apply hasDerivAt_const
+      · simp only [hasDerivAt_deriv_iff]
+        apply DifferentiableAt.pow
+        apply DifferentiableAt.const_sub
+        apply DifferentiableAt.const_mul differentiableAt_id
+      · suffices (1 - a * x) ^ (m + 1) > 0 by linarith
+        apply pow_pos
+        nlinarith
+    · simp_rw [← Function.iterate_succ_apply', Polynomial.iterate_derivative_mul, Polynomial.eval_finset_sum]
+      simp only [Nat.succ_eq_add_one, nsmul_eq_mul, eval_mul, eval_natCast]
+      apply ContinuousOn.intervalIntegrable_of_Icc (by norm_num)
+      apply continuousOn_finset_sum
+      intro i _
+      apply ContinuousOn.mul continuousOn_const
+      · apply ContinuousOn.mul
+        · simp_rw [Polynomial.iterate_derivative_X_pow_eq_smul]
+          simp only [eval_smul, eval_pow, eval_X, smul_eq_mul]
+          apply ContinuousOn.mul continuousOn_const (ContinuousOn.pow continuousOn_id _)
+        · rw [show (1 - X : ℝ[X]) ^ n = (X ^ n : ℝ[X]).comp (1 - X) by simp,
+            Polynomial.iterate_derivative_comp_one_sub_X (p := X ^ n),
+            Polynomial.iterate_derivative_X_pow_eq_smul]
+          simp only [smul_comp, pow_comp, X_comp, Algebra.mul_smul_comm, eval_smul, eval_mul,
+            eval_pow, eval_neg, eval_one, eval_sub, eval_X, smul_eq_mul, ge_iff_le, zero_le_one,
+            Set.uIcc_of_le]
+          refine ContinuousOn.mul continuousOn_const (ContinuousOn.mul continuousOn_const ?_)
+          apply ContinuousOn.pow (ContinuousOn.sub continuousOn_const continuousOn_id)
+    · have (x : ℝ) : deriv (deriv^[m] fun x ↦ 1 / (1 - a * x)) x = Function.eval x (deriv (deriv^[m] fun x ↦ 1 / (1 - a * x))) := by
+        simp only [one_div, Function.eval]
+      simp_rw [this]
+      simp_rw [← Function.iterate_succ_apply', Nat.succ_eq_add_one, Function.eval]
+      apply ContinuousOn.intervalIntegrable_of_Icc (by norm_num)
+      simp_rw [n_derivative]
+      apply ContinuousOn.div continuousOn_const
+      · apply ContinuousOn.pow (ContinuousOn.sub continuousOn_const (ContinuousOn.mul continuousOn_const continuousOn_id))
+      · intro x hx
+        simp only [Set.mem_Icc] at hx
+        suffices (1 - a * x) ^ (m + 1 + 1) > 0 by linarith
+        apply pow_pos
+        nlinarith
+
+lemma integral_legendre_mul_smooth_eq {n : ℕ} {a : ℝ} (h : 0 < a ∧ a < 1):
+  ∫ (x : ℝ) in (0)..1, eval x (legendre n) * (fun x ↦ 1 / (1 - a * x)) x =
+  (- 1) ^ n / n ! * ∫ (x : ℝ) in (0)..1, x ^ n * (1 - x) ^ n * (deriv^[n] fun x ↦ 1 / (1 - a * x)) x := by
+  simp only [eval_mul, one_div, eval_C]
+  simp_rw [mul_assoc, intervalIntegral.integral_const_mul]
+  rw [← mul_one_div, one_div]
+  nth_rw 4 [mul_comm]
+  rw [mul_assoc]
+  congr
+  obtain h := integral_legendre_mul_smooth_eq_aux (n := n) (a := a) (m := n) (by norm_num) h
+  simp only [one_div, ge_iff_le, le_refl, tsub_eq_zero_of_le, Function.iterate_zero, id_eq,
+    eval_mul, eval_pow, eval_X, eval_sub, eval_one] at h
+  rw [h]
+  simp_rw [← mul_assoc]
+
+lemma legendre_integral_special {a : ℝ} (ha : 0 < a ∧ a < 1) : ∫ (x : ℝ) in (0)..1, eval x (legendre n) / (1 - a * x) =
+    (-1) ^ n * ∫ (x : ℝ) in (0)..1, x ^ n * (1 - x) ^ n * a ^ n / (1 - a * x) ^ (n + 1) := by
+  have : ∫ (x : ℝ) in (0)..1, eval x (legendre n) / (1 - a * x) =
+    ∫ (x : ℝ) in (0)..1, eval x (legendre n) * (1 / (1 - a * x)) := by
+    rw [intervalIntegral.integral_of_le (by norm_num), intervalIntegral.integral_of_le (by norm_num),
+      MeasureTheory.integral_Ioc_eq_integral_Ioo, MeasureTheory.integral_Ioc_eq_integral_Ioo]
+    apply MeasureTheory.setIntegral_congr (by simp)
+    intro _ _
+    group
+  rw [this, integral_legendre_mul_smooth_eq ha, ← intervalIntegral.integral_const_mul,
+    ← intervalIntegral.integral_const_mul, intervalIntegral.integral_of_le (by norm_num),
+    intervalIntegral.integral_of_le (by norm_num), ← MeasureTheory.integral_Icc_eq_integral_Ioc,
+    ← MeasureTheory.integral_Icc_eq_integral_Ioc]
+  apply MeasureTheory.setIntegral_congr (by simp)
+  intro x hx
+  simp only
+  rw [n_derivative]
+  simp_all only [Set.mem_Ioo, Set.mem_Icc]
+  rw [← mul_assoc, mul_div, mul_div, div_eq_div_iff]
+  · rw [mul_assoc, mul_assoc, mul_assoc, mul_comm, mul_div]
+    symm
+    rw [eq_div_iff]
+    · ring
+    · norm_cast
+      exact Nat.factorial_ne_zero n
+  · suffices (1 - a * x) ^ (n + 1) > 0 by linarith
+    apply pow_pos
+    nlinarith
+  · suffices (1 - a * x) ^ (n + 1) > 0 by linarith
+    apply pow_pos
+    nlinarith
